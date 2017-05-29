19466f6dfcb647c8b074d045aadbbe88d554bff0
justinegeo96 <justinegeo96@gmail.com>
2017-05-29 15:46:04 +0530

    removed unnecessary whitespaces and comments.

--- face_extraction.py
+++ face_extraction.py
@@ -1,34 +1,16 @@
-import cv2
-
+# load dataset to numpy arrays, later to be trained.
 import numpy as np
 import os
 from PIL import Image
 from scipy import misc
 from skimage import io
 
-
-# face = misc.face()
-# misc.imsave('face.png', face)
-
-# using scipy
-# face = misc.imread('dataset/obama_rgb.jpg')
-# print(face)
-
-# #using scikit-image
-# face = io.imread('dataset/face_obama.png', as_grey=True)
-
-
 # initialise a 4D array.
 faceRepo = misc.imread('dataset/mammootty0new.jpg')
 faceRepo = np.array([faceRepo])
 
-# print(faceRepo.shape)
-
-
 # initialise label array. (supervised learning).
-faceLabelRepo = np.array([10]) # 12 for mammootty
-# print(faceLabelRepo.shape)
-#
+faceLabelRepo = np.array([10]) # 10 for mammootty
 
 # traverse through images in dataset.
 directories = [x[0] for x in os.walk('dataset') if x[0] != 'dataset']
@@ -44,9 +26,7 @@ for peopleFolder in directories:
     for image in listImages:
         # this is the RGB image array.
         face = misc.imread(peopleFolder + '/' + image)
-
         faceRepo = np.append(faceRepo, [face],axis=0)
-
         #assigning label
         bit = peopleFolder[8:]   # peoplefolder = "dataset/<fodler_name>" , folder name starting from index 8.
         if bit == 'dulquer':
@@ -67,16 +47,4 @@ for peopleFolder in directories:
             label = int(16)
         else:
             pass
-
         faceLabelRepo = np.append(faceLabelRepo, [label],axis=0)
-
-
-img = cv2.imread('dataset/mammootty0new.jpg', 0)
-cv2.imshow('image',img)
-
-# # # show dimens.
-# print(faceRepo.shape)
-# print(faceLabelRepo.shape)
-# # #
-# print(faceLabelRepo)
-# print(listOfCelebrities)
